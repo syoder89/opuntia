62c8bada740e0fcc9ed1eb4b425cfd82d1e7f4de
syoder <syoder@imagestream.com>
2013-09-03 16:21:34 -0500

    Update IPSec interface

--- imagestream/package/ipsec/files/usr/lib/lua/luci/controller/ipsec.lua
+++ imagestream/package/ipsec/files/usr/lib/lua/luci/controller/ipsec.lua
@@ -1,5 +1,5 @@
 --[[
-LuCI - Lua Configuration Interface
+LuCI - Lua Configuration IPSEC
 
 Copyright 2013 Juan Flores <juan.flores@gdc-cala.com.mx>
 
@@ -9,16 +9,105 @@ You may obtain a copy of the License at
 
 	http://www.apache.org/licenses/LICENSE-2.0
 
-$Id: ipsec.lua 9558 2013-08-19 13:58:22Z jow $
+$Id: 
 ]]--
 
 module("luci.controller.ipsec", package.seeall)
 
 function index()
+	local page
 	if not nixio.fs.access("/etc/config/ipsec_pre") then
 		return
 	end
 
-	page = entry({"admin", "network", "ipsec"}, cbi("ipsec"), _("IPSEC"))
+	page = node("admin", "network", "ipsec")
+	page.target = cbi("ipsec/overview")
+	page.title  = _("IPSEC")
+	page.index  = true
 
+	page = entry({"admin", "network", "ipsec", "logs"}, call("ipsec_logs"), "IPSEC Logs")
+
+	page = entry({"admin", "network", "ipsec", "tunnel_form"}, cbi("ipsec/tunnel_form", {autoapply=true}), nil)
+	page.leaf = true
+
+	page = entry({"admin", "network", "ipsec", "tunnel_delete"}, call("tunnel_delete"), nil)
+	page.leaf = true
+
+	page = entry({"admin", "network", "ipsec", "tunnel_status"}, call("tunnel_status"), nil)
+	page.leaf = true
+
+	page = entry({"admin", "network", "ipsec", "tunnel_reconnect"}, call("tunnel_reconnect"), nil)
+	page.leaf = true
+
+	page = entry({"admin", "network", "ipsec", "tunnel_shutdown"}, call("tunnel_shutdown"), nil)
+	page.leaf = true
+
+end
+
+function ipsec_logs()
+	local ipm = require "luci.model.ipsec".init()
+	local ip_xfrm_state = ipm:ip_xfrm_state()
+	local ip_xfrm_policy = ipm:ip_xfrm_policy()
+	local iptables = ipm:iptables()
+	luci.template.render("ipsec/logs", 
+		{ip_xfrm_state=ip_xfrm_state, ip_xfrm_policy=ip_xfrm_policy, iptables=iptables})
+end
+
+function tunnel_delete(tunnel)
+	local ipm = require "luci.model.ipsec".init()
+	local tun = ipm:del_tunnel(tunnel)
+	if tun then
+		luci.sys.call("env -i /usr/sbin/ipsec down remote_%q-%q >/dev/null 2>/dev/null" %{tunnel, tunnel})
+		ipm:commit("ipsec_pre")
+		luci.http.redirect(luci.dispatcher.build_url("admin/network/ipsec"))
+		return
+	end
+
+	luci.http.status(404, "No such tunnel")
+end
+
+function tunnel_reconnect(tunnel)
+	local ipm = require "luci.model.ipsec".init()
+	local tun = ipm:get_tunnel(tunnel)
+	if tun then
+		luci.sys.call("env -i /usr/sbin/ipsec up remote_%q-%q >/dev/null 2>/dev/null" %{tunnel, tunnel})
+		luci.http.status(200, "Reconnected")
+		return
+	end
+
+	luci.http.status(404, "No such tunnel")
+end
+
+function tunnel_shutdown(tunnel)
+	local ipm = require "luci.model.ipsec".init()
+	local tun = ipm:get_tunnel(tunnel)
+	if tun then
+		luci.sys.call("env -i /usr/sbin/ipsec down remote_%q-%q >/dev/null 2>/dev/null" %{tunnel, tunnel})
+		luci.http.status(200, "Shutdown")
+		return
+	end
+
+	luci.http.status(404, "No such tunnel")
+end
+
+function tunnel_status(tunnels)
+	local ipm = require "luci.model.ipsec".init()
+	local rv   = { }
+
+	local tunnel
+	for tunnel in tunnels:gmatch("[%w%.%-_]+") do
+		local stat = ipm:get_tunnel_status(tunnel)
+		rv[#rv+1] = {
+			name = tunnel,
+			status = stat.status,
+		}
+	end
+	
+	if #rv > 0 then
+		luci.http.prepare_content("application/json")
+		luci.http.write_json(rv)
+		return
+	end
+
+	luci.http.status(404, "No such device")
 end
