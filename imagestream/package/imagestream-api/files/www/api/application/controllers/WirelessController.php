a4cf603857c9ad8275824bbe0a7e2b57fa13c955
syoder <syoder@imagestream.com>
2014-05-30 16:45:36 -0500

    Changes per Juan

--- imagestream/package/imagestream-api/files/www/api/application/controllers/WirelessController.php
+++ imagestream/package/imagestream-api/files/www/api/application/controllers/WirelessController.php
@@ -10,14 +10,46 @@ class WirelessController extends ControllerBase{
 	}
 	
     //Accion index
-    public function info(){
-        $data['output'] = $this->model->getInfo();
+    public function getInfo(){
+    	$interface = $_GET['interface'];
+        $data['output'] = $this->model->getInfo($interface);
         $this->_view->show('common/json.php', $data);
     }
     
-    public function scan(){
-    	$radio = $_GET['radio'];
-    	$data['output'] = $this->model->getScan($radio);
+    public function getScan(){
+    	$interface = $_GET['interface'];
+    	$data['output'] = $this->model->getScan($interface);
+    	$this->_view->show('common/json.php', $data);
+    }
+    
+    public function getClients(){
+    	$interface = $_GET['interface'];
+    	$data['output'] = $this->model->getClients($interface);
+    	$this->_view->show('common/json.php', $data);
+    }
+    
+    public function getStats(){
+    	$interface = $_GET['interface'];
+    	$data['output'] = $this->model->getStats($interface);
+    	$this->_view->show('common/json.php', $data);
+    }
+
+    public function getTxPower(){
+    	$interface = $_GET['interface'];
+    	$data['output'] = $this->model->getTxPower($interface);
+    	$this->_view->show('common/json.php', $data);
+    }
+    
+    public function getTxPowerList(){
+    	$interface = $_GET['interface'];
+    	$data['output'] = $this->model->getTxPowerList($interface);
+    	$this->_view->show('common/json.php', $data);
+    }
+
+    public function setTxPower(){
+    	$interface = $_GET['interface'];
+    	$txPower = $_GET['txPower'];
+    	$data['output'] = $this->model->setTxPower($interface, $txPower);
     	$this->_view->show('common/json.php', $data);
     }
 }
--- imagestream/package/imagestream-api/files/www/api/application/models/WirelessModel.php
+++ imagestream/package/imagestream-api/files/www/api/application/models/WirelessModel.php
@@ -1,6 +1,9 @@
 <?php
 class WirelessModel extends ModelBase{
-
+	
+	const BIN_PATH = '/usr/bin/';
+	const SBIN_PATH = '/sbin/';
+	
 	private function _cleanKey($key){
 		$notAllowed = array(' ', '(', ')', ':');
 		$replace = array('_', '', '', '');
@@ -9,15 +12,19 @@ class WirelessModel extends ModelBase{
 		return $key;
 	}
 	
-    public function getInfo(){
+    public function getInfo($interface){
     	$result = array();
     	$currentInterface = '';
+
+        if($interface){
+    		$output = shell_exec(self::BIN_PATH . "iwinfo {$interface} info");
+    	}else{
+    		$output = shell_exec(self::BIN_PATH . 'iwinfo');
+    	}
     	
-    	$output = shell_exec("/tmp/iwinfo");
     	$lines = explode("\n", $output);
     	
     	foreach($lines as $line){
-    		//echo $line, '<br>';
     		if(preg_match('/^(\S+)\b(\s+)(\S+)\b:(\s+)(\S+)/', $line, $matches)){
     			$currentInterface = $matches[1];
     			$options = array('name' => $currentInterface, $this->_cleanKey($matches[3]) => trim($matches[5], '"'));
@@ -34,26 +41,25 @@ class WirelessModel extends ModelBase{
     	return $result; 
     }
     
-    public function getScan($radio){
+    public function getScan($interface){
     	$result = array();
     	$output = array();
-    	if($radio){
-    		$output[$this->_cleanKey($radio)] = shell_exec("/tmp/iwinfo {$radio} scan");
+    	if($interface){
+    		$output[$this->_cleanKey($interface)] = shell_exec(self::BIN_PATH . "iwinfo {$interface} scan");
     	}else{
-    		$information = $this->getInfo();
-    		if(count($information) > 0){
-    			foreach($information as $info){
-    				$output[$this->_cleanKey($info['name'])] = shell_exec("/tmp/iwinfo {$info['name']} scan");
+    		$interfaces = $this->getInfo();
+    		if(count($interfaces) > 0){
+    			foreach($interfaces as $info){
+    				$output[$this->_cleanKey($info['name'])] = shell_exec(self::BIN_PATH . "iwinfo {$info['name']} scan");
     			}
     		}
     	}
     	
-    	foreach($output as $radio => $_output){
-    		$result[$radio] = array();
-    		$lines = explode("\n", $_output);
+    	foreach($output as $interface => $scan){
+    		$result[$interface] = array();
+    		$lines = explode("\n", $scan);
     		$currentCell = '';
     		foreach($lines as $line){
-    			//echo $line, '<br>';
     			if(preg_match('/^(\S+\s\S+)\s-\s(\S+)\b:(\s+)(\S+)/', $line, $matches)){
     				$currentCell = $this->_cleanKey($matches[1]);
     				$options = array('name' => $currentCell, $this->_cleanKey($matches[2]) => trim($matches[4]));
@@ -64,10 +70,149 @@ class WirelessModel extends ModelBase{
     				$options[$this->_cleanKey($matches[2][0])] = trim(trim($matches[3][0]), '"');
     			}
     			if($currentCell){
-    				$result[$radio][$currentCell] = $options;
+    				$result[$interface][$currentCell] = $options;
     			}
     		}    		
     	}
     	return $result;
     }
+    
+    public function getClients($interface){
+    	$result = array();
+    	$output = array();
+    	
+    	if($interface){
+    		$output[$this->_cleanKey($interface)] = shell_exec(self::BIN_PATH . "iw {$interface} station dump");
+    	}else{
+    		$interfaces = $this->getInfo();
+    		if(count($interfaces) > 0){
+    			foreach($interfaces as $info){
+    				$output[$this->_cleanKey($info['name'])] = shell_exec(self::BIN_PATH . "iw {$info['name']} station dump");
+    			}
+    		}
+    	}
+    	 
+    	foreach($output as $interface => $dump){
+    		$result[$interface] = array();
+    		$lines = explode("\n", $dump);
+    		$currentStation = '';
+    		foreach($lines as $line){
+    			if(preg_match('/^Station\s(\S+)\s\(on\s(\S+)\)/', $line, $matches)){
+    				$currentStation = 's' . $this->_cleanKey($matches[1]);
+    				$options = array('macaddr' => $matches[1], 'interface' => $matches[2]);
+    			}elseif(preg_match_all('/^(\s+)\b(.*):\s+(.*)$/', $line, $matches)){
+    				$options[$this->_cleanKey($matches[2][0])] = trim($matches[3][0]);
+    			}
+    			if($currentStation){
+    				$result[$interface][$currentStation] = $options;
+    			}
+    		}    		
+    	}
+    	return $result;
+    }
+    
+    public function getStats($interface){
+    	$result = array();
+    	$output = array();
+
+    	if($interface){
+    		$output[$this->_cleanKey($interface)] = shell_exec("devstatus {$interface}");
+    	}else{
+    		$interfaces = $this->getInfo();
+    		if(count($interfaces) > 0){
+    			foreach($interfaces as $info){
+    				$output[$this->_cleanKey($info['name'])] = shell_exec("devstatus {$info['name']}");
+    			}
+    		}
+    	}
+
+    	foreach($output as $interface => $stats){
+    		$result[$interface] = (array)json_decode($stats);
+    	}
+    	return $result;
+    }
+
+    public function getTxPower($interface){
+    	$result = array();
+    	$output = array();
+
+    	if($interface){
+    		$output[$this->_cleanKey($interface)] = shell_exec(self::BIN_PATH . "iwinfo {$interface} txpowerlist");
+    	}else{
+    		$interfaces = $this->getInfo();
+    		if(count($interfaces) > 0){
+    			foreach($interfaces as $info){
+    				$output[$this->_cleanKey($info['name'])] = shell_exec(self::BIN_PATH . "iwinfo {$info['name']} txpowerlist");
+    			}
+    		}
+    	}
+
+    	foreach($output as $interface => $dump){
+    		$result[$interface] = array();
+    		$lines = explode("\n", $dump);
+    		$currentTxPower = '';
+    		foreach($lines as $line){
+    			if(preg_match('/^\*(.+)/', $line, $matches)){
+    				$result[$interface] = trim($matches[1]);
+    			}
+    		}    		
+    	}
+    	return $result;
+    }
+
+    public function getTxPowerList($interface){
+    	$result = array();
+
+    	if(!$interface){
+    		return 'Error: <interface> is missing';
+    	}
+    	
+		$output = shell_exec(self::BIN_PATH . "iwinfo {$interface} txpowerlist");
+		$lines = explode("\n", $output);
+		foreach($lines as $line){
+			if($line){
+				$result[] = trim($line);
+			}
+    	}
+    	return $result;
+    }
+
+    public function setTxPower($interface, $txPower){
+    	$result = array();
+    	$validTxPower = false;
+
+        if(!$interface){
+    		return array('success' => false, 'error' => '<interface> is missing');
+    	}
+
+		if($txPower == ''){
+			return array('success' => false, 'error' => '<txPower> is missing');
+    	}
+    	
+    	$txPowerList = $this->getTxPowerList($interface);
+    	if(count($txPowerList) > 0){
+    		foreach($txPowerList as $txPowerValue){
+    		    if(preg_match("/(.*){$txPower} dBm/", $txPowerValue)){
+    				$validTxPower = true;
+    				break;
+    			}
+    			
+    		}
+    	}
+    	
+    	if(!$validTxPower){
+    		return array('success' => false, 'error' => '<txPower> is not valid');
+    	}
+    	
+    	$infoInterface = $this->getInfo($interface);
+    	$radio = str_replace('phy', 'radio', $infoInterface[$interface]['phy_name']);
+
+    	shell_exec("uci set wireless.{$radio}.txpower={$txPower}");
+    	shell_exec("uci commit");
+    	shell_exec(self::SBIN_PATH . "wifi reload {$radio}");
+    	sleep(2);
+    	$newTxPower = $this->getTxPower($interface);
+		return array('success' => true, 'txPower' => $newTxPower[$interface]);
+    }
+    
 }
